4151651631aca5c0372daf51b256ad1dd861a126
Mimoun Bijjou <mimounbijjou@Mimouns-Air.local>
2019-03-26 12:14:42 +0100

    add offer to project settings

--- letus/settings.py
+++ letus/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'offer'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,20 @@ WSGI_APPLICATION = 'letus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
--- offer/models.py
+++ offer/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-# class Offer(models.Model):
-#     title = models.CharField(max_length=200, null=True, blank=True)
-#     sub_title = models.CharField(max_length=200, null=True, blank=True)
+class Offer(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+    sub_title = models.CharField(max_length=200, null=True, blank=True)
