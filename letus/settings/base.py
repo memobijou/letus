ad743ec3981caf524e0ee6bd9f942d3584e341f5
Mimoun Bijjou <mimounbijjou@Mimouns-Air.local>
2019-03-26 22:38:46 +0100

    one folder up

--- letus/settings/base.py
+++ letus/settings/base.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print(f"SETTINGS HELLO: {BASE_DIR}")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -133,5 +134,5 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../static'),
 )
