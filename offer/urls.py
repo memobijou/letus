3834ceb4ef1631645676b85c587e1260b09bb93b
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-03-31 19:23:14 +0200

    remove all implicit endpoint

--- offer/urls.py
+++ offer/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from offer.viewsets import OfferViewset, OfferAPIView
+from django.urls import path
+from offer.viewsets import OfferAPIView
 
-router = DefaultRouter()
-router.register("offers", OfferViewset)
 
 urlpatterns = [
-    path("", include(router.urls),),
-    path("offers/creation", OfferAPIView.as_view())
+    path("offers/", OfferAPIView.as_view())
 ]
--- offer/viewsets.py
+++ offer/viewsets.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, mixins
-from abc import ABCMeta, abstractmethod
+from rest_framework import mixins
 
 from rest_framework.generics import GenericAPIView
 from rest_framework.pagination import LimitOffsetPagination
@@ -7,24 +6,10 @@ from offer.models import Offer
 from offer.serializers import OfferSerializer
 
 
-class BaseOfferViewset(metaclass=ABCMeta):
-    queryset = Offer.objects.all()
-
-    @property
-    @abstractmethod
-    def serializer_class(self):
-        pass
-
-    pagination_class = LimitOffsetPagination
-
-
-class OfferViewset(BaseOfferViewset, viewsets.ModelViewSet):
-    serializer_class = OfferSerializer
-
-
 class OfferAPIView(mixins.CreateModelMixin, mixins.ListModelMixin, GenericAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
+    pagination_class = LimitOffsetPagination
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
